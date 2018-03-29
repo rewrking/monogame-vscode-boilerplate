0d384e6a2a8628c204b2fbed1d0b33fec037c3c2
Andrew King <andrew.king@nvoicepay.com>
2018-03-29 16:34:00 -0700

    updated Sprite & Transformable

--- src/Graphics/Sprite.cs
+++ src/Graphics/Sprite.cs
@@ -4,11 +4,13 @@ using Microsoft.Xna.Framework.Content;
 
 namespace MonoGame
 {
-	class Sprite
+	class Sprite : Transformable
 	{
 
 		private Texture2D texture;
 		private Rectangle textureRect;
+		private Rectangle destRect;
+		private Color color;
 		/*
 			Vertex m_vertices[4];
 			const Texture* m_texture;
@@ -17,31 +19,43 @@ namespace MonoGame
 
 		public Sprite()
 		{
+			this.color = Color.White;
 		}
 
 		public void SetTexture(ref Texture2D texture, bool resetRect = false)
 		{
+			this.texture = texture;
 			if (resetRect) {
-				Rectangle newRect = new Rectangle(0, 0, texture.Width, texture.Height);
+				Rectangle newRect = new Rectangle(0, 0, this.texture.Width, this.texture.Height);
 				SetTextureRect(ref newRect);
 			}
-
-			this.texture = texture;
-
 		}
 		public void SetTextureRect(ref Rectangle rectangle)
 		{
 			if (rectangle != this.textureRect)
 			{
 				this.textureRect = rectangle;
+				Resize();
 				//UpdatePositions();
 				//UpdateTexCoords();
 			}
 		}
 
-		public void SetColor()
+		public void Resize()
+		{
+			this.destRect = new Rectangle(
+				this.position.X,
+				this.position.Y,
+				this.position.X + (int)(this.texture.Width * this.scale.X),
+				this.position.Y + (int)(this.texture.Height * this.scale.Y)
+			);
+		}
+
+		public void SetColor(Color color)
 		{
 			//this.texture.Bounds.Top
+			if (this.color != color)
+				this.color = color;
 		}
 
 		//===============================================
@@ -49,18 +63,19 @@ namespace MonoGame
 		{
 		}
 
-		public void LoadContent(ref ContentManager content, string textureName, bool resetRect = false)
+		public void LoadContent(ContentManager content, string textureName)
 		{
 			Texture2D newTexture = content.Load<Texture2D>(textureName);
-			SetTexture(ref newTexture, resetRect);
+			SetTexture(ref newTexture, true);
 		}
 
 		public void Update(ref GameTime gameTime)
 		{
 		}
 
-		public void Draw(ref GameTime gameTime)
+		public void Draw(ref SpriteBatch spriteBatch)
 		{
+			spriteBatch.Draw(this.texture, this.destRect, this.textureRect, this.color, this.rotation, this.origin, SpriteEffects.None, 0);
 		}
 	}
 }
